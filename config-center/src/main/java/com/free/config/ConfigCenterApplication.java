fb1edbc764bb4505804e02e1bf652510c80ebf2d
leoxu1719 <leoxu1719@outlook.com>
2018-12-13 15:26:51 +0800

    基于spring cloud config 的配置中心 新增配置文件demo

--- config-center/src/main/java/com/free/config/ConfigCenterApplication.java
+++ config-center/src/main/java/com/free/config/ConfigCenterApplication.java
@@ -1,8 +1,10 @@
 package com.free.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigCenterApplication {
 
     public static void main(String[] args){
