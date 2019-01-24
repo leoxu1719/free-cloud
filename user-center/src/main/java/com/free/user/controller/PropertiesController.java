1b7a08fe6c9f9e22c35325ac3187abf2ef5e6752
xwc <tiyin1718>
2019-01-24 16:46:49 +0800

    配置中心，动态刷新配置

--- user-center/src/main/java/com/free/user/controller/PropertiesController.java
+++ user-center/src/main/java/com/free/user/controller/PropertiesController.java
@@ -1,20 +1,30 @@
 package com.free.user.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/property")
+@RefreshScope
+@Api(value = "Propertites Controller", tags = {"配置检测接口"})
 public class PropertiesController {
 
-    @Value("${spring.cloud.config.server.default-label}")
+    //@Value("${spring.cloud.config.server.default-label}")
     private String label;
+
+    @Value("${title}")
+    private String title;
 
 
     @GetMapping(value = "/label")
     public String getLabel(){
         return label;
     }
+
+    @GetMapping(value = "/title")
+    public String getTitle(){return title;}
 }
